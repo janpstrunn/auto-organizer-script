e4447afe03088b7dca834811f319ed79677e2c63
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-13 17:25:37 +0000

    Update auto-organizer-script.sh

--- auto-organizer-script.sh
+++ auto-organizer-script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 source_dir="$1"
-destination_dir="$2"
+dst_dir="$2"
 
-[[ -d "$destination_dir/Media/Images" ]] || mkdir -p "$destination_dir/Media/Images"
-[[ -d "$destination_dir/Documents" ]] || mkdir -p "$destination_dir/Documents"
-[[ -d "$destination_dir/Others" ]] || mkdir -p "$destination_dir/Others"
-[[ -d "$destination_dir/Media/Videos" ]] || mkdir -p "$destination_dir/Media/Videos"
-[[ -d "$destination_dir/Media/Gifs" ]] || mkdir -p "$destination_dir/Media/Gifs"
-[[ -d "$destination_dir/Applications" ]] || mkdir -p "$destination_dir/Applications"
-[[ -d "$destination_dir/Torrents" ]] || mkdir -p "$destination_dir/Torrents"
-[[ -d "$destination_dir/Media/Music" ]] || mkdir -p "$destination_dir/Media/Music"
-[[ -d "$destination_dir/Documents/Text" ]] || mkdir -p "$destination_dir/Documents/Text"
+[[ -d "$dst_dir/Media/Images" ]] || mkdir -p "$dst_dir/Media/Images"
+[[ -d "$dst_dir/Documents" ]] || mkdir -p "$dst_dir/Documents"
+[[ -d "$dst_dir/Others" ]] || mkdir -p "$dst_dir/Others"
+[[ -d "$dst_dir/Media/Videos" ]] || mkdir -p "$dst_dir/Media/Videos"
+[[ -d "$dst_dir/Media/Gifs" ]] || mkdir -p "$dst_dir/Media/Gifs"
+[[ -d "$dst_dir/Applications" ]] || mkdir -p "$dst_dir/Applications"
+[[ -d "$dst_dir/Torrents" ]] || mkdir -p "$dst_dir/Torrents"
+[[ -d "$dst_dir/Media/Music" ]] || mkdir -p "$dst_dir/Media/Music"
+[[ -d "$dst_dir/Documents/Text" ]] || mkdir -p "$dst_dir/Documents/Text"
 
 find "$source_dir" -type f -exec bash -c '
-    destination_dir="$1"
+    dst_dir="$1"
     case "$0" in
-        *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$0" "$destination_dir/Media/Images/" ;;
-        *.gif) mv "$0" "$destination_dir/Media/Gifs/" ;;
-        *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$0" "$destination_dir/Applications/" ;;
-        *.torrent) mv "$0" "$destination_dir/Torrents/" ;;
-        *.text|*.md|*.html) mv "$0" "$destination_dir/Documents/Text/" ;;
-        *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$0" "$destination_dir/Documents/" ;;
-        *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$0" "$destination_dir/Media/Videos/" ;;
-        *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$0" "$destination_dir/Media/Music/" ;;
-        *) mv "$0" "$destination_dir/Others/" ;;
+        *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$0" "$dst_dir/Media/Images/" ;;
+        *.gif) mv "$0" "$dst_dir/Media/Gifs/" ;;
+        *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$0" "$dst_dir/Applications/" ;;
+        *.torrent) mv "$0" "$dst_dir/Torrents/" ;;
+        *.text|*.md|*.html) mv "$0" "$dst_dir/Documents/Text/" ;;
+        *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$0" "$dst_dir/Documents/" ;;
+        *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$0" "$dst_dir/Media/Videos/" ;;
+        *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$0" "$dst_dir/Media/Music/" ;;
+        *) mv "$0" "$dst_dir/Others/" ;;
     esac
-' {} "$destination_dir" \;
+' {} "$dst_dir" \;
 
 echo "Complete!"
