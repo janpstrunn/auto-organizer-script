c6dc4e59260aec66f4985fb7d31bec8f5edd1b9b
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-14 10:58:55 +0000

    Update auto-organizer-script.sh

--- auto-organizer-script.sh
+++ auto-organizer-script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-source="$1"
-destiny="$2"
+dir1="$1"
+dir2="$2"
 
-[[ -d "$destiny/Media/Images" ]] || mkdir -p "$destiny/Media/Images"
-[[ -d "$destiny/Documents" ]] || mkdir -p "$destiny/Documents"
-[[ -d "$destiny/Others" ]] || mkdir -p "$destiny/Others"
-[[ -d "$destiny/Media/Videos" ]] || mkdir -p "$destiny/Media/Videos"
-[[ -d "$destiny/Media/Gifs" ]] || mkdir -p "$destiny/Media/Gifs"
-[[ -d "$destiny/Applications" ]] || mkdir -p "$destiny/Applications"
-[[ -d "$destiny/Torrents" ]] || mkdir -p "$destiny/Torrents"
-[[ -d "$destiny/Media/Music" ]] || mkdir -p "$destiny/Media/Music"
-[[ -d "$destiny/Documents/Text" ]] || mkdir -p "$destiny/Documents/Text"
+[[ -d "$dir2/Media/Images" ]] || mkdir -p "$dir2/Media/Images"
+[[ -d "$dir2/Documents" ]] || mkdir -p "$dir2/Documents"
+[[ -d "$dir2/Others" ]] || mkdir -p "$dir2/Others"
+[[ -d "$dir2/Media/Videos" ]] || mkdir -p "$dir2/Media/Videos"
+[[ -d "$dir2/Media/Gifs" ]] || mkdir -p "$dir2/Media/Gifs"
+[[ -d "$dir2/Applications" ]] || mkdir -p "$dir2/Applications"
+[[ -d "$dir2/Torrents" ]] || mkdir -p "$dir2/Torrents"
+[[ -d "$dir2/Media/Music" ]] || mkdir -p "$dir2/Media/Music"
+[[ -d "$dir2/Documents/Text" ]] || mkdir -p "$dir2/Documents/Text"
 
-find "$source" -type f -exec bash -c '
-    destiny="$1"
+find "$dir1" -type f -exec bash -c '
+    dir2="$1"
     case "$0" in
-        *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$0" "$destiny/Media/Images/" ;;
-        *.gif) mv "$0" "$destiny/Media/Gifs/" ;;
-        *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$0" "$destiny/Applications/" ;;
-        *.torrent) mv "$0" "$destiny/Torrents/" ;;
-        *.text|*.md|*.html) mv "$0" "$destiny/Documents/Text/" ;;
-        *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$0" "$destiny/Documents/" ;;
-        *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$0" "$destiny/Media/Videos/" ;;
-        *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$0" "$destiny/Media/Music/" ;;
-        *) mv "$0" "$destiny/Others/" ;;
+        *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$0" "$dir2/Media/Images/" ;;
+        *.gif) mv "$0" "$dir2/Media/Gifs/" ;;
+        *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$0" "$dir2/Applications/" ;;
+        *.torrent) mv "$0" "$dir2/Torrents/" ;;
+        *.text|*.md|*.html) mv "$0" "$dir2/Documents/Text/" ;;
+        *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$0" "$dir2/Documents/" ;;
+        *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$0" "$dir2/Media/Videos/" ;;
+        *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$0" "$dir2/Media/Music/" ;;
+        *) mv "$0" "$dir2/Others/" ;;
     esac
-' {} "$destiny" \;
+' {} "$dir2" \;
 
 echo "Complete!"
