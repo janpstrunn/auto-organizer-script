b06d239874818f9636f6310e35be1766139754fe
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-14 10:59:12 +0000

    Update auto-organizer-ui.sh

--- auto-organizer-ui.sh
+++ auto-organizer-ui.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-source_dir=$(zenity --file-selection --directory --title="Select Source Directory")
-destination_dir=$(zenity --file-selection --directory --title="Select Destination Directory")
+dir1=$(zenity --file-selection --directory --title="Select Source Directory")
+dir2=$(zenity --file-selection --directory --title="Select Destination Directory")
 
-if [ -z "$source_dir" ] || [ -z "$destination_dir" ]; then
+if [ -z "$dir1" ] || [ -z "$dir2" ]; then
     zenity --error --text="Please select both source and destination directories."
     exit 1
 fi
 
-[[ -d "$destination_dir/Media/Images" ]] || mkdir -p "$destination_dir/Media/Images"
-[[ -d "$destination_dir/Documents" ]] || mkdir -p "$destination_dir/Documents"
-[[ -d "$destination_dir/Others" ]] || mkdir -p "$destination_dir/Others"
-[[ -d "$destination_dir/Media/Videos" ]] || mkdir -p "$destination_dir/Media/Videos"
-[[ -d "$destination_dir/Media/Gifs" ]] || mkdir -p "$destination_dir/Media/Gifs"
-[[ -d "$destination_dir/Applications" ]] || mkdir -p "$destination_dir/Applications"
-[[ -d "$destination_dir/Torrents" ]] || mkdir -p "$destination_dir/Torrents"
-[[ -d "$destination_dir/Media/Music" ]] || mkdir -p "$destination_dir/Media/Music"
-[[ -d "$destination_dir/Documents/Text" ]] || mkdir -p "$destination_dir/Documents/Text"
+[[ -d "$dir2/Media/Images" ]] || mkdir -p "$dir2/Media/Images"
+[[ -d "$dir2/Documents" ]] || mkdir -p "$dir2/Documents"
+[[ -d "$dir2/Others" ]] || mkdir -p "$dir2/Others"
+[[ -d "$dir2/Media/Videos" ]] || mkdir -p "$dir2/Media/Videos"
+[[ -d "$dir2/Media/Gifs" ]] || mkdir -p "$dir2/Media/Gifs"
+[[ -d "$dir2/Applications" ]] || mkdir -p "$dir2/Applications"
+[[ -d "$dir2/Torrents" ]] || mkdir -p "$dir2/Torrents"
+[[ -d "$dir2/Media/Music" ]] || mkdir -p "$dir2/Media/Music"
+[[ -d "$dir2/Documents/Text" ]] || mkdir -p "$dir2/Documents/Text"
 
 (
   echo "0"
-  find "$source_dir" -type f | while read -r file; do
+  find "$dir1" -type f | while read -r file; do
     case "$file" in
-      *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$file" "$destination_dir/Media/Images/" ;;
-      *.gif) mv "$file" "$destination_dir/Media/Gifs/" ;;
-      *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$file" "$destination_dir/Applications/" ;;
-      *.torrent) mv "$file" "$destination_dir/Torrents/" ;;
-      *.text|*.md|*.html) mv "$file" "$destination_dir/Documents/Text/" ;;
-      *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$file" "$destination_dir/Documents/" ;;
-      *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$file" "$destination_dir/Media/Videos/" ;;
-      *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$file" "$destination_dir/Media/Music/" ;;
-      *) mv "$file" "$destination_dir/Others/" ;;
+      *.jpg|*.png|*.webp|*.jpeg|*.bmp|*.tiff|*.svg|*.raw|*.jpeg) mv "$file" "$dir2/Media/Images/" ;;
+      *.gif) mv "$file" "$dir2/Media/Gifs/" ;;
+      *.AppImage|*.exe|*.sh|*.dmg|*.deb|*.rpm|*.msi|*.pkg|*.zip|*.tar|*.tar.gz|*.tar.bz2) mv "$file" "$dir2/Applications/" ;;
+      *.torrent) mv "$file" "$dir2/Torrents/" ;;
+      *.text|*.md|*.html) mv "$file" "$dir2/Documents/Text/" ;;
+      *.pdf|*.doc|*.docx|*.odt|*.epub|*.rtf) mv "$file" "$dir2/Documents/" ;;
+      *.mp4|*.mov|*.mkv|*.avi|*.mpeg) mv "$file" "$dir2/Media/Videos/" ;;
+      *.mp3|*.ogg|*.aac|*.wav|*.flac|*.ape|*.aiff|*.pcm|*.opus) mv "$file" "$dir2/Media/Music/" ;;
+      *) mv "$file" "$dir2/Others/" ;;
     esac
   done
  echo "Complete!"
